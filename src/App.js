4bd27d2869631d4fcffd287b2116e77fd1662ef5
Pramod Suthar <pramodsuthar@mail.com>
2021-08-25 12:02:30 -0300

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Setup from './tutorial/1-useState/setup/3-useState-array';
+
+
 function App() {
+  let title = 'random title';
+
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <Setup />
     </div>
-  )
-}
+  );
+};
 
 export default App
