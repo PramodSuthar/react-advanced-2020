aa0b9ed08cae7919bae1dbd975c46de6b2d7b5e3
PramodSuthar <67489670+PramodSuthar@users.noreply.github.com>
2021-08-29 10:33:29 -0300

    .

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setshow] = useState(false)
+  return (<>
+    <button className="btn" onClick={() => setshow(!show)}>show/hide</button>
+    {show && <Item />}
+  </>
+  );
+};
+
+const Item = () => {
+  const [size, setsize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setsize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>window</h1>
+      <h2>size : {size} px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
